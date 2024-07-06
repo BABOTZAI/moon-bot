d7491584ae47bb23c61b6a326289d32d2d33931f
BABOTZAI <147415086+BABOTZAI@users.noreply.github.com>
2024-07-06 10:16:14 +0700

    Update tools-remini.js

--- plugins/tools-remini.js
+++ plugins/tools-remini.js
@@ -6,11 +6,11 @@ let handler = async (m, {
   try {
     let q = m.quoted ? m.quoted : m
     let mime = (q.msg || q).mimetype || ''
-    if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`Send or reply to images with commands : *"${usedPrefix + command}"*`)
+    if (!/image\/(jpe?g|png)/.test(mime)) return m.reply(`Kirim atau balas gambar dengan perintah ${usedPrefix + command}`)
     let media = await q.download()
     let url = await scrap.uploader(media)
-    m.react('🕒')
-    let json = await Func.fetchJson(API('alya', '/api/enhance', {
+    m.reply(status.wait)
+    let json = await Func.fetchJson(API('alya', '/api/remini', {
       image: url.data.url
     }, 'apikey'))
     if (!json.status) return m.reply(Func.jsonFormat(json))
@@ -20,7 +20,7 @@ let handler = async (m, {
     return m.reply(Func.jsonFormat(e))
   }
 }
-handler.help = handler.command = ['remini']
+handler.help = handler.command = ['hdr']
 handler.tags = ['tools']
 handler.limit = 1
 module.exports = handler
